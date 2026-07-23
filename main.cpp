76a78c3023c6aac538684f2022a7f9641d4eb25f
ocha-- <140893815+ocha545@users.noreply.github.com>
2026-07-23 09:50:35 +0900

    add: 関数の役割をコメントで明記しました。

--- main.cpp
+++ main.cpp
@@ -3,17 +3,17 @@
 
 int main()
 {
-	SW_Init();
-	SW_Size(960, 720);
-	SW_Title(L"たいとる");
-	SW_BackColor(0, 255, 0);
-	SW_CreateWindow();
-	SW_DarkMode();
-	SW_Show();
+	SW_Init();					// (必須!)座標や色などのデータを初期化
+	SW_Size(960, 720);			// ウィンドウサイズの設定
+	SW_Title(L"たいとる");		// ウィンドウタイトルの設定
+	SW_BackColor(0, 255, 0);	// 背景色の設定(R, G, B)
+	SW_CreateWindow();			// (必須!)ウィンドウの作成
+	SW_DarkMode();				// ダークモードの有効化
+	SW_Show();					// ウィンドウの表示
 
-	while (SW_Update())
+	while (SW_Update())			// ウィンドウを更新
 	{
 	}
 
-	SW_Close();
+	SW_Close();					// ウィンドウを閉じる
 }
